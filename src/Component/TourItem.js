ae24a88ea3fd9fa1c0bf3f52d9b283771211e2fe
Neelshah768 <neelfenilshah@gmail.com>
2021-07-31 13:45:13 +0530

    updated files

--- src/Component/TourItem.js
+++ src/Component/TourItem.js
@@ -2,18 +2,20 @@ import Card from "../UI/Card";
 import classes from './TourItem.module.css';
 
 const TourItem = (props) => {
+
   const userList = props.data.map((items) => {
+    
     return (
       <Card className={classes.tour}>
-        <div>
+        <div >
           <img src={items.image} alt="paris" />
         </div>
-        <div className={classes.description}>
+        <div className={classes.description} >
           <span className={classes.name}>{items.name}</span>
           <span className={classes.price}>${items.price}</span>
           <div className={classes.info}>{items.info}</div>
         </div>
-        <button>Not Intrested</button>
+        <button >Not Intrested</button>
       </Card>
     );
   });
